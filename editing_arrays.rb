3ed4954ddb909196e4dbe3f69ef170f7c09f7d46
briannaliu <briannaliu3@gmail.com>
2018-07-30 14:38:50 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -6,9 +6,9 @@ def change_rainbow_colors
   rainbow_colors[0] = "red"
   # puts rainbow_colors[0]
   rainbow_colors[1] = "light_red"
-  puts rainbow_colors[1]
+  # puts rainbow_colors[1]
   rainbow_colors[2] = "light_yellow"
-  puts rainbow_colors[2]
+  # puts rainbow_colors[2]
 end
 
 puts change_rainbow_colors
